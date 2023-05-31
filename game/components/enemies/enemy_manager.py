625cc0481c9b5ceb1140731aa390ef85e6a479ca
VallejosAdrian <Adrian.Vallejos@fundacion-jala.org>
2023-05-31 17:55:53 -0400

    Second-Class-Homewrok
    Reutilze la clase 'enemy' para poder ejecutar un nuevo enemigo y que esta tenga movimiento independiente

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,6 +1,6 @@
 from game.components.enemies.enemy import Enemy
-
-
+from game.components.enemies.enemy2 import Enemy2
+from game.utils.constants import SCREEN_HEIGHT
 
 class EnemyManager():
     def __init__(self):
@@ -11,12 +11,25 @@ class EnemyManager():
 
         for enemy in self.enemies:
             enemy.update(self.enemies)
+            self.enemy2_remove(enemy)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
-    
+
+    # Verifica que 'Enemy2' este en la pantalla 
+    def enemy2_remove(self, enemy):
+        if isinstance(enemy, Enemy2) and enemy.rect.y > SCREEN_HEIGHT:
+            self.enemies.remove(enemy)
+
     def add_enemy(self):
         if len(self.enemies) < 1:
             enemy = Enemy()
+            enemy_a = Enemy()
             self.enemies.append(enemy)
+            self.enemies.append(enemy_a)
+
+        # Veirfica que 'Enemy2' se ecuentre en la lista, no obstante la añadira
+        elif not any(isinstance(enemy, Enemy2) for enemy in self.enemies):
+            enemy2 = Enemy2()
+            self.enemies.append(enemy2)
